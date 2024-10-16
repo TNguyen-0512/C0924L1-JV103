c7f2d1f877ff096713907f793e3a217700fa9b1d
TNguyen-0512 <btn8176@gmail.com>
2024-10-16 22:02:54 +0700

    baitap

--- ss7_bien_kieu_du_lieu_toan_tu/bai_tap/doi_tien.js
+++ ss7_bien_kieu_du_lieu_toan_tu/bai_tap/doi_tien.js
@@ -4,10 +4,10 @@ function ChangeMoney() {
     let To = document.getElementById("To").value;
     let Result;
     if (FromC == "USD" && To == "VND") {
-        Result = "Result: " + (Amount * 23000) + " Đ";
+        Result = "Result: " + (Amount * 24.990) + " Đ";
     }
     else if (FromC == "VND"&& To =="USD"){
-        Result = "Result: "+ (Amount / 23000) + " $";
+        Result = "Result: "+ (Amount / 24.990) + " $";
     }
     else if (FromC == "VND") {
         Result = "Result: " + Amount + " Đ"
